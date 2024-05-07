ea62069893825e9c8273113f4c940186fd407919
Milad244 <milad.abdi244@gmail.com>
2024-05-06 17:48:29 -0700

    Drawing with Text Hand In

--- General Programming/src/Main.java
+++ General Programming/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
-        System.out.println((9 % 5) - 8 + Math.pow(2,3) + (Math.sqrt(9) / 3));
+        String courseName = "Computer Programming 11 - VLN";
+        String fullName = "Milad Karel Abdi";
+        String madeByText = "This was made by";
+        String dateOnText = "On the date of";
+        String currentDate = "06-05-2024";
+        String longLine = "--------------------------------------------------------------------------------------------";
+        String mediumLine = "--------------------------------";
+        String shortLine = "----------------";
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(shortLine + "\t\t\t\t\\" + courseName + "\\\t\t\t\t" + shortLine);
+        System.out.println(shortLine + "\t\t\t\t\t " + madeByText + "\t\t\t\t\t\t" + shortLine);
+        System.out.println(shortLine + "\t\t\t\t\t\"" + fullName + "\"\t\t\t\t\t\t" + shortLine);
+        System.out.println(shortLine + "\t\t\t\t\t  " + dateOnText + "\t\t\t\t\t\t" + shortLine + "\n" + shortLine + "\t\t\t\t\t\t" + currentDate + "\t\t\t\t\t\t\t" + shortLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(mediumLine + "-----------/////--------" + "/////---------------" + shortLine);
+        System.out.println(mediumLine + "-----------(())---------" + "(())" + mediumLine);
+        System.out.println(mediumLine + "------------++++++++++++++--" + mediumLine);
+        System.out.println(mediumLine + "------------++++++++++++++--" + mediumLine);
+        System.out.println(mediumLine + "------------++++++++++++++--" + mediumLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
+        System.out.println(longLine);
     }
 }
